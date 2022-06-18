7d8ad93433c17cef734336c00b9b237f7f018362
Gilbert Samama <gsamama@gmail.com>
2022-06-18 19:03:04 +0300

    running version, just understanding the difference between warn and error in code

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const logger = require('./system/logger');
 
 const app = express();
 
-logger.add(console.log('test'));
+// logger.log('info', 'all good');
+// logger.log('error', 'no good');
+let dt = new Date();
+logger.info(dt.toJSON().toString() + ' >>> all good <<<');
+
+logger.error(dt.toJSON().toString() + '*** no good ***');
+
+logger.warn(dt.toJSON().toString() + '### warn ###')
 
 app.listen(3000);
--- system/logger/index.js
+++ system/logger/index.js
@@ -1,15 +1,23 @@
-const winston = require('winston');
+const {createLogger, transports, format, config} = require('winston');
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  
-  defaultMeta: {service: 'user-service'},
-  transports:[
-    new winston.transports.File({filename: 'error.log'}, {level: 'error'}),
-    new winston.transports.File({filename:'combined.log'}),
-    new winston.transports.Console(),
+const transports1 = {
+  file1: new transports.File({ filename: './logs/info.log' , level: 'info' }),
+  file2: new transports.File({ filename: './logs/error.log', level: 'error' }),
+  file3: new transports.File({ filename: './logs/warning.log', level: 'warn' }),  
+};
+
+format.combine(
+  format.colorize(),
+  format.json()
+);
+
+const logger = createLogger({
+  format: format.json(),
+  transports: [
+    transports1.file1,
+    transports1.file2,
+    transports1.file3,    
   ]
-})
+});
 
 module.exports= logger;
